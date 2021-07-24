761882e60868d0fe5f692f7a1ff9b9be2e870884
Your Name <you@example.com>
2021-07-24 15:00:23 +0200

    init

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>welcome</h1>
+      <h1 className=" bg-red-400 m-1 p-5 place-content-center flex items-center text-lg text-cyan-500 font-bold">welcome</h1>
     </div>
   );
 }
